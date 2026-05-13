6dbc9a1d57d6cdc4c1f44e201ca768ad97eb7666
haiduong149 <dichonline.vn@gmail.com>
2026-05-13 15:01:56 +0700

    CI_CD_practice committed successfully

---
+++ src/P01/CI_CD_001.java
@@ -0,0 +1,22 @@
+package P01;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CI_CD_001 {	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			//LOG IN
+			driver.get("https://rahulshettyacademy.com/client");
+			driver.findElement(By.id("userEmail")).sendKeys("laidiephaiduong@gmail.com");
+			driver.findElement(By.id("userPassword")).sendKeys("seleniumframework");
+			driver.findElement(By.id("login")).click();
+			driver.close();
+
+	}
+
+}
