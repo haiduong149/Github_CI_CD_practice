774c9dc969667e30234f0f86afe60689df16132c
haiduong149 <dichonline.vn@gmail.com>
2026-05-13 17:24:57 +0700

    add wait

--- src/test/java/com/CI_CD_Test001.java
+++ src/test/java/com/CI_CD_Test001.java
@@ -10,10 +10,7 @@ public class CI_CD_Test001 {
 	@Test
 		public void loginTest() throws InterruptedException, IOException { 
 			// TODO Auto-generated method stub
-		/*org.openqa.selenium.chrome.ChromeOptions options = new org.openqa.selenium.chrome.ChromeOptions();
-		options.addArguments("--headless"); 
-		options.addArguments("--disable-gpu");
-		options.addArguments("--window-size=1920,1080");*/
+		
 		WebDriver driver = new ChromeDriver();
 		
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -22,6 +19,7 @@ public class CI_CD_Test001 {
 			driver.findElement(By.id("userEmail")).sendKeys("laidiephaiduong@gmail.com");
 			driver.findElement(By.id("userPassword")).sendKeys("seleniumframework");
 			driver.findElement(By.id("login")).click();
+			Thread.sleep(3000); 
 			driver.close();
 
 	}
