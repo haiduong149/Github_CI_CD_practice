c217e67157d5928dfd09af1426f0c1776838fc27
haiduong149 <dichonline.vn@gmail.com>
2026-05-13 17:17:35 +0700

    update pom

--- src/test/java/com/CI_CD_Test001.java
+++ src/test/java/com/CI_CD_Test001.java
@@ -10,8 +10,12 @@ public class CI_CD_Test001 {
 	@Test
 		public void loginTest() throws InterruptedException, IOException { 
 			// TODO Auto-generated method stub
+		/*org.openqa.selenium.chrome.ChromeOptions options = new org.openqa.selenium.chrome.ChromeOptions();
+		options.addArguments("--headless"); 
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");*/
+		WebDriver driver = new ChromeDriver();
 		
-			WebDriver driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			//LOG IN
 			driver.get("https://rahulshettyacademy.com/client");
