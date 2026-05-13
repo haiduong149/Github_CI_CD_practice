9116550943ae332633759f28ca9937cbcfa480be
haiduong149 <dichonline.vn@gmail.com>
2026-05-13 15:31:13 +0700

    update @Test

--- src/P01/CI_CD_001.java
+++ src/P01/CI_CD_001.java
@@ -3,11 +3,13 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class CI_CD_001 {	
+	@Test
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-
+		
 			WebDriver driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			//LOG IN
